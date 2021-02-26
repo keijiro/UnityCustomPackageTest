f1707a842f9fbb2f919576d32870b32fb8b0bdc2
Keijiro Takahashi <keijiro@gmail.com>
2021-02-26 14:29:12 +0900

    Move package contents from upm branch to master

---
+++ Packages/jp.keijiro.unity-custom-package-test/README.md
@@ -0,0 +1,4 @@
+UnityCustomPackageTest
+======================
+
+This is a small project for testing the custom package features of Unity Package Manager.
---
+++ Packages/jp.keijiro.unity-custom-package-test/Runtime/SimpleRotation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace UnityCustomPackageTest
+{
+    class SimpleRotation : MonoBehaviour
+    {
+        public float angularVelocity = 10;
+
+        void Update()
+        {
+            var rot = Quaternion.AngleAxis(angularVelocity * Time.deltaTime, Vector3.up);
+            transform.localRotation = rot * transform.localRotation;
+        }
+    }
+}
